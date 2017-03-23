42cae039f9f2d63ecdc999d52683882d40c2480d
SimonaStavarache <simona.stavarache96@yahoo.com>
2017-03-23 11:30:13 +0200

    Update Destinatii.java

--- Destinatii.java
+++ Destinatii.java
@@ -2,7 +2,7 @@ import java.util.Vector;
 
 public class Destinatii {
 
-  public numeDestinatie String;
+  private String numeDestinatie ;
 
     private Vector  destination;
     private Vector  myPachete;
