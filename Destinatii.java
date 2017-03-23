b92c4bacdf0dc207db14b45bbeb57e5cd1e1d3f9
SimonaStavarache <simona.stavarache96@yahoo.com>
2017-03-23 11:26:48 +0200

    Add files via upload

---
+++ Destinatii.java
@@ -0,0 +1,33 @@
+import java.util.Vector;
+
+public class Destinatii {
+
+  public numeDestinatie String;
+
+    private Vector  destination;
+    private Vector  myPachete;
+    private Vector  myPachete;
+    private Vector  myPreturi;
+    private Pachete myPachete;
+    private Preturi myPreturi;
+    private Vector  myInterfataAplicatie;
+    /**
+   * 
+   * @element-type Turist
+   */
+    private Hotel myHotel;
+    private Vector  myGhid;
+    private Ghid myGhid;
+    private Vector  1..*;
+    private Vector  myBD;
+  
+  public void getInfo() {
+  }
+
+  public void Destinatii() {
+  }
+
+  public void setDestinatie() {
+  }
+
+}
