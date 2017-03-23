8c63dfe8eb9e588b8f956cbc88e38081516e34d8
AdrianDorneanu <adrian.dorneanu96@gmail.com>
2017-03-23 11:32:15 +0200

    Add files via upload

---
+++ mijlocDeCalatorie.java
@@ -0,0 +1,37 @@
+import java.util.Vector;
+
+public class mijlocDeCalatorie {
+
+    private String nrMatricol;
+
+    private int dataPlecare;
+
+    private String numeDestinatie ;
+
+    private String numeTransport;
+
+    private int nrLocuri;
+
+    private Vector  myInterfataAplicatie;
+    /**
+     *
+     * @element-type Turist
+     */
+    private Vector  transport;
+    private Vector  myBD;
+    /**
+     *
+     * @element-type Destinatii
+     */
+    private Vector  myDestinatii;
+
+    public void getDataPlecare() {
+    }
+
+    public void getOraPlecare() {
+    }
+
+    public void mijlocDeCalatorie() {
+    }
+
+}
