a36290383ddccea556fb4fe6df2cd3ddab6da73d
Morosanu George-Cosmin <georgekosmingeorge@yahoo.com>
2017-03-23 11:28:00 +0200

    InterfataAplicatiei

---
+++ InterfataAplicatie.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class InterfataAplicatie {
+
+    private Vector  account;
+    private Vector  destination;
+    private Vector  myClient;
+    private Vector  myDestinatii;
+    private Vector  mymijlocDeCalatorie;
+    private Destinatii myDestinatii;
+    private Vector  myBD;
+    private Vector  myController;
+
+  public void InterfataAplicatiei() {
+  }
+
+}
