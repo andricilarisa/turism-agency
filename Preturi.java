3209e635f2930b59443011f4d50a7257fb60684a
deR4Fa <razavnvs@gmail.com>
2017-03-23 11:36:57 +0200

    Add files via upload

---
+++ Preturi.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class Preturi {
+
+  public pret int;
+
+    public Vector  myDestinatii;
+    public Destinatii myDestinatii;
+    public Vector  myBD;
+
+  public void getPrice() {
+  }
+
+  public void Preturi() {
+  }
+
+}
---
+++ Turist.java
@@ -0,0 +1,27 @@
+import java.util.Vector;
+
+public class Turist extends Client {
+
+  public numeDestinatie String;
+
+  public numeTransport String;
+
+  public perioadaSejur date;
+
+    public mijlocDeCalatorie transport;
+    public Destinatii Locatie;
+    public Vector  myBD;
+
+  public void getDestination() {
+  }
+
+  public void getTransport() {
+  }
+
+  public void getPerioadaSejur() {
+  }
+
+  public void Turist() {
+  }
+
+}
