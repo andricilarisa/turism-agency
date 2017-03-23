3bdd30a4ae67d69a9a1430933fe36cf2dab1086d
andricilarisa <larisa.andrici@gmail.com>
2017-03-23 11:31:25 +0200

    Add files via upload

---
+++ BD.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Vector;
+
+public class BD {
+
+    public Vector  myInterfataAplicatie;
+    public Vector  myPersoana;
+    public Vector  myTurist;
+    public Vector  myDestinatii;
+    public Vector  mymijlocDeCalatorie;
+    public Vector  myPachete;
+    public Vector  myPreturi;
+    public Vector  myHotel;
+    public Vector  myGhid;
+
+    public void BD() {
+    }
+
+}
