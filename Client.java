96abe2192c549eda5f24cdd0593b5bc986846efc
siq10 <robert_ilie_vicol@yahoo.ro>
2017-03-23 11:28:41 +0200

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class Client extends Persoana {
+
+  public String UserNameg;
+
+  public  String Parola;
+
+  public String contBancar ;
+
+  public int ID;
+
+    public Vector  account;
+    public Vector  myInterfataAplicatie;
+
+  public void getInfo() {
+  }
+
+  public void Client() {
+  }
+
+}
---
+++ Pachete.java
@@ -0,0 +1,26 @@
+import java.util.Vector;
+
+public class Pachete {
+
+  public  int pretPromotional;
+
+  public  String Informatii ;
+
+  public String numePachet ;
+
+  public String recenzii ;
+
+    public Vector  myDestinatii;
+    /**
+   * 
+   * @element-type Destinatii
+   */
+    public Vector  myBD;
+
+  public void getInfo() {
+  }
+
+  public void Pachet() {
+  }
+
+}
